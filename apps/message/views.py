05ce08d7c3867432cfe0003cc569e1fab81f82de
Eggsy <464063606@qq.com>
2017-08-21 19:10:54 +0800

    Another DjPr Finished

--- apps/message/views.py
+++ apps/message/views.py
@@ -3,8 +3,31 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import UserMessage
+
 # Create your views here.
 
 
 def getform(request):
-    return render(request, 'message_form.html')
+    message = None
+    all_messages = UserMessage.objects.filter(name='eggsy')
+    if all_messages:
+        message = all_messages[0]
+    # all_messages.delete()
+
+    # if request.method == 'POST':
+    #     name = request.POST.get('name', '')
+    #     message = request.POST.get('message', '')
+    #     address = request.POST.get('address', '')
+    #     email = request.POST.get('email', '')
+    #     user_message = UserMessage()
+    #     user_message.name = name
+    #     user_message.message = message
+    #     user_message.address = address
+    #     user_message.email = email
+    #     user_message.object_id = '1'
+    #     user_message.save()
+
+    return render(request, 'message_form.html', {
+        "my_message": message
+    })
